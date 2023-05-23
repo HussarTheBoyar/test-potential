5249099068dcb0ffee7b375f89c65f198cb31780
HussarTheBoyar <102010596+HussarTheBoyar@users.noreply.github.com>
2023-05-23 23:26:42 +0700

    Test potential

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -0,0 +1,114 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.*;
+import com.udacity.jwdnd.course1.cloudstorage.services.*;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    private final FileService fileService;
+    private final UserService userService;
+    private final NoteService noteService;
+    private final CredentialService credentialService;
+    private final EncryptionService encryptionService;
+
+    public HomeController(
+            FileService fileService, UserService userService, NoteService noteService,
+            CredentialService credentialService, EncryptionService encryptionService) {
+        this.fileService = fileService;
+        this.userService = userService;
+        this.noteService = noteService;
+        this.credentialService = credentialService;
+        this.encryptionService = encryptionService;
+    }
+
+    @GetMapping
+    public String getHomePage(
+            Authentication authentication, @ModelAttribute("newFile") FileForm newFile,
+            @ModelAttribute("newNote") NoteForm newNote, @ModelAttribute("newCredential") CredentialForm newCredential,
+            Model model) {
+        Integer userId = userService.getUserIdByAuthen(authentication);
+
+        if (userId == -1){
+            return "login";
+        }
+
+        model.addAttribute("files", this.fileService.getFileListings(userId));
+        model.addAttribute("notes", noteService.getNoteListings(userId));
+        model.addAttribute("credentials", credentialService.getCredentialListings(userId));
+        model.addAttribute("encryptionService", encryptionService);
+
+        return "home";
+    }
+
+
+    @PostMapping("/addingFile")
+    public String newFile(
+            Authentication authentication, @ModelAttribute("newFile") FileForm newFile,
+            @ModelAttribute("newNote") NoteForm newNote, @ModelAttribute("newCredential") CredentialForm newCredential, Model model) throws IOException {
+        int userId = userService.getUserIdByAuthen(authentication);
+
+        if (userId == -1){
+            return "login";
+        }
+
+        int checkUpdate = 0;
+        List<String> fileListings = fileService.getFileListings(userId);
+        MultipartFile multipartFile = newFile.getFile();
+        String fileName = multipartFile.getOriginalFilename();
+        boolean fileIsDuplicate = false;
+        for (String fileListing : fileListings) {
+            if (fileListing.equals(fileName)) {
+                fileIsDuplicate = true;
+
+                break;
+            }
+        }
+        if (!fileIsDuplicate) {
+            checkUpdate = fileService.addFile(multipartFile, authentication.getName());;
+            if(checkUpdate == 0){
+                model.addAttribute("result", "not-saved");
+            } else {
+                model.addAttribute("result", "success");
+            }
+        } else {
+            model.addAttribute("result", "error");
+            model.addAttribute("message", "You have tried to add a duplicate file.");
+        }
+        model.addAttribute("files", fileService.getFileListings(userId));
+
+        return "result";
+    }
+
+    @GetMapping(
+            value = "/getFile/{fileName}",
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+    )
+    public @ResponseBody
+    byte[] getFile(@PathVariable String fileName) {
+        return fileService.getFile(fileName).getFileData();
+    }
+
+    @GetMapping(value = "/deletFile/{fileName}")
+    public String deleteFile(
+            Authentication authentication, @PathVariable String fileName, @ModelAttribute("newFile") FileForm newFile,
+            @ModelAttribute("newNote") NoteForm newNote, @ModelAttribute("newCredential") CredentialForm newCredential,
+            Model model) {
+        fileService.deleteFile(fileName);
+        Integer userId = userService.getUserIdByAuthen(authentication);
+        model.addAttribute("files", fileService.getFileListings(userId));
+        model.addAttribute("result", "success");
+
+        return "result";
+    }
+}
